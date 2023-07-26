eae64b6ee16700611faaadeed976958fb7369fca
Juggy-45 <ahmedburhan.lm10@gmail.com>
2023-07-26 18:04:55 +0500

    changes

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import Fellows from "./components/fellows";
+import './index.css'
+function App() {
+  return (
+    <div className="bg">
+     <Fellows/>
+    </div>
+  );
+}
+
+export default App;
