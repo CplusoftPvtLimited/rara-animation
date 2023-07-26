6d33f93cc52172e16407741b351c242b7a4f1de2
Juggy-45 <ahmedburhan.lm10@gmail.com>
2023-07-26 18:02:41 +0500

    changes

---
+++ server/config/db.js
@@ -0,0 +1,19 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('ahmed', 'root', 'Total123', {
+  host: 'localhost',
+  dialect: 'mysql',
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log(
+      'Connection to the database has been established successfully.'
+    );
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+module.exports = sequelize;
---
+++ server/models/Profile.js
@@ -0,0 +1,77 @@
+// models/BlogPost.js
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+// const Blog = require('./Blog');
+
+const Profile = sequelize.define('Profile', {
+  name: {
+    type: DataTypes.STRING,
+    // allowNull: false,
+    // validate: {
+    //   notEmpty: true,
+    // },
+  },
+  nameEnglish: {
+    type: DataTypes.STRING,
+    // allowNull: false,
+    // validate: {
+    //   notEmpty: true,
+    // },
+  },
+  tagLine: {
+    type: DataTypes.TEXT,
+    // allowNull: false,
+    // validate: {
+    //   notEmpty: true,
+    // },
+  },
+  imagePath: {
+    type: DataTypes.STRING,
+    // allowNull: true,
+  },
+  jobPost: {
+    type: DataTypes.STRING
+    // allowNull: false 
+    //validate: { notEmpty: true }
+  },
+  profileDesc: {
+    type: DataTypes.TEXT
+    // allowNull: false 
+    //validate: { notEmpty: true }
+  },
+  websiteUrl: {
+    type: DataTypes.STRING,
+    // allowNull: true
+  },
+  heading: {
+    type: DataTypes.TEXT,
+    // allowNull: false,
+    // validate: {
+    //   notEmpty: true,
+    // },
+  },
+  paragraph: {
+    type: DataTypes.TEXT,
+    // allowNull: false,
+    // validate: {
+    //   notEmpty: true,
+    // },
+  },
+  featuredImage: {
+    type: DataTypes.STRING,
+    // allowNull: true,
+  },
+  pictureSlider: {
+    type: DataTypes.JSON, 
+    // allowNull: true,
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW,
+  },
+});
+
+// Profile.hasMany(Blog); 
+// Blog.belongsTo(Profile);
+
+module.exports = Profile;
