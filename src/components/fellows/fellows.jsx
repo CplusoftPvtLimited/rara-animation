3f8cb4e514c9500c48b82db5c26649d3a3241714
Juggy-45 <ahmedburhan.lm10@gmail.com>
2023-07-26 19:59:43 +0500

    Fellow section

--- src/components/fellows/fellows.jsx
+++ src/components/fellows/fellows.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './index.css'
 import {useState, useEffect} from 'react'
-
+import Pic from '../../assets/imgaes/pic.jpg'
 function Fellows() {
 
 
@@ -13,6 +13,7 @@ function Fellows() {
             const response = await fetch ('http://localhost:4500/api/profile/getAllProfiles')
             const data = await response.json();
             setFellowsData(data.profiles);
+            console.log('*************Data', fellowsData)
         }
      catch (err) {
         console.error('Error fetching fellows data:', err);
@@ -23,7 +24,7 @@ function Fellows() {
       
   return (
     <section className='mt-[45px]'>
-         {/************************** Fellow Section ******************************/}
+         {/************************** RAAR Fellow Section ******************************/}
          <div className='top-bar flex justify-between'>
             <div className='flex'>
             <div class="bullet-point"></div>
@@ -34,16 +35,61 @@ function Fellows() {
 
          {/************************** Profile Section ******************************/}
 
-         <div>
+         <div >
+         <div className="fellows-container w-[85%] mx-auto">
         {fellowsData.length === 0 ? (
           <p>Loading...</p>
         ) : (
-         fellowsData.map((fellow) => (
-            <div> 
-                Hii
+          fellowsData.map((fellow) => (
+            <div key={fellow.id} className="fellow-box">
+              <div>
+              <img className="thumbnail" src={Pic} alt=""  />
+              </div>
+              <div className='mt-[25px] pl-[40px]'>
+              <h4 >{fellow.name}</h4>
+              <p >{fellow.nameEnglish}</p>
+
+              <h6 className='tag'>{fellow.tagLine}</h6>
+              </div>
             </div>
-         ))
+            
+          ))
         )}
+        </div>
+      </div>
+
+       {/************************** RARA Associate Fellow Section ******************************/}
+       <div className='top-bar flex justify-between'>
+            <div className='flex'>
+            <div class="bullet-point"></div>
+                 <h2 className='rara-text'>RARAフェロー</h2> 
+           </div>
+            <h3 className='my-auto'>16 RARA FELLOWS</h3>
+         </div>
+
+           {/************************** Assosiate Profile Section ******************************/}
+
+           <div >
+         <div className="fellows-container w-[85%] mx-auto">
+        {fellowsData.length === 0 ? (
+          <p>Loading...</p>
+        ) : (
+          fellowsData.map((fellow) => (
+            <div key={fellow.id} className="fellow-box">
+              <div>
+              <img className="thumbnail" src={Pic} alt=""  />
+              </div>
+              <div className='mt-[25px] pl-[40px]'>
+              <h4 >{fellow.name}</h4>
+              <p >{fellow.nameEnglish}</p>
+
+              <h6 className='tag'>{fellow.tagLine}</h6>
+              </div>
+            </div>
+            
+          ))
+        )}
+        </div>
       </div>
     </section>
   )
