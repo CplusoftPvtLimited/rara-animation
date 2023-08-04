b2df07a200637974db7a1e80688294b1d5fd2940
Juggy-45 <ahmedburhan.lm10@gmail.com>
2023-08-04 11:20:55 +0500

    Updates

--- src/components/News/card.jsx
+++ src/components/News/card.jsx
@@ -1,97 +1,155 @@
 import React from "react";
 import "./index.css";
 import Image from "../../assets/images/news-image.jpg";
-import {useState, useEffect } from 'react'
-function Card() {
-const [newsData, setNewsData] = useState([])
-const [currentPage, setCurrentPage] = useState(1);
-const itemsPerPage = 21;
-useEffect (() => {
-    const fetchNews = async() => {
-        const response = await fetch ('http://localhost:4500/api/news/getAllNews')
-        const data = await response.json()
-        setNewsData(data.items)
-    }
-    fetchNews()
-}, [])
+import { useState, useEffect } from "react";
 
-function truncateText(text, maxLength) {
+function Card() {
+  const [newsData, setNewsData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [sortOption, setSortOption] = useState("latest");
+  const [sortedNewsData, setSortedNewsData] = useState([]);
+  const itemsPerPage = 21;
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+
+  useEffect(() => {
+    const fetchNews = async () => {
+      const response = await fetch("http://localhost:4500/api/news/getAllNews");
+      const data = await response.json();
+      setNewsData(data.items);
+      setSortedNewsData(data.items);
+    };
+    fetchNews();
+  }, []);
+
+  // Cut short the blog title
+  function truncateText(text, maxLength) {
     if (text.length > maxLength) {
       return text.slice(0, maxLength) + "...";
     }
     return text;
   }
 
-  const startIndex = (currentPage - 1) * itemsPerPage;
-  const endIndex = startIndex + itemsPerPage;
+  // Scroll page to top when page changes
+  const scrollToTop = () => {
+    window.scrollTo(500, 500);
+  };
+
+  // Filter Buttons Setup
+  const filterNewsData = (option) => {
+    switch (option) {
+      case "latest":
+        setSortedNewsData([...newsData].sort((a, b) => b.pubDate - a.pubDate));
+        break;
+      case "oldest":
+        setSortedNewsData(
+          [...newsData].sort(
+            (a, b) => new Date(a.isoDate) - new Date(b.isoDate)
+          )
+        );
+        break;
+      default:
+        setSortedNewsData(newsData);
+        break;
+    }
+  };
+
+  // Handle Active Button
+  const handleSortingOption = (option) => {
+    setSortOption(option);
+    filterNewsData(option);
+  };
 
   return (
     <section>
-      <div className="filter-1 flex mt-[150px] mx-auto w-[80%] py-[50px] gap-32 pr-[150px]">
+      <div className="filter-1 mt-[75px] lg:mt-[150px] mx-auto w-[80%] py-[50px] gap-32 pr-[150px lg:flex ">
         <div className="filter-by-date my-auto">
-          <h5>Sort by date</h5>
+          <h5>Sort by</h5>
         </div>
-        <div className="flex gap-8">
-          <button className="filter-btn">All News</button>
-          <button className="filter-btn">Latests</button>
-          <button className="filter-btn">Old to new</button>
-          <button className="filter-btn">更新情報</button>
-        </div>
-      </div>
-
-      <div className="filter-2 flex mx-auto w-[80%] py-[50px] gap-32 pr-[150px]">
-        <div className="filter-by-date my-auto">
-          <h5>Sort by date</h5>
-        </div>
-        <div className="flex gap-8">
-          <button className="filter-btn">All News</button>
-          <button className="filter-btn">Latests</button>
-          <button className="filter-btn">Old to new</button>
-          <button className="filter-btn">更新情報</button>
+        <div className="flex gap-8 mt-[25px] lg:mt-auto">
+          <button
+            className={`filter-btn ${sortOption === "latest" ? "active" : " "}`}
+            onClick={() => handleSortingOption("latest")}
+          >
+            Latests
+          </button>
+          <button
+            className={`filter-btn ${sortOption === "oldest" ? "active" : " "}`}
+            onClick={() => handleSortingOption("oldest")}
+          >
+            Old to new
+          </button>
+          <button
+            className={`filter-btn ${
+              sortOption === "更新情報" ? "active" : " "
+            }`}
+            onClick={() => handleSortingOption("更新情報")}
+          >
+            更新情報
+          </button>
         </div>
       </div>
 
       {/***********************Cards ***************************/}
-      <div className="Cards mt-[50px] mb-[100px] w-[80%] m-auto" id="card">
-        <div className="flex flex-wrap justify-between">
-            {newsData.slice(startIndex, endIndex).map((news, index) => (
-                <a href={news.link}>
-          <div className="news-card mt-[70px] cursor-pointer">
-            <div className="img-box">
-              <img className="img" src={Image} alt="" />
-            </div>
-            <div className="news-cat my-auto" key={index}>
-              <h5>研究活動レポート / 松村 浩由</h5>
-            </div>
-            <div className="mt-[30px]">
-            <p>{truncateText(news.title, 85)}</p>
-            </div>
-            <div className="date-box flex mt-[90px] justify-between">
-              <div>
-                <p className="date">{new Date(news.isoDate).toLocaleDateString()}</p>
-              </div>
-              <div> <a href={news.link}>
-                <h5 className="text-[10px] lg:text-[11px]">VIEW DETAILS</h5>
-                </a>
-              </div>
-            </div>
+      {newsData.length > 0 ? (
+        <div className="Cards mt-[50px] mb-[100px] w-[80%] m-auto" id="card">
+          <div className="flex flex-wrap justify-between">
+            {console.log(sortOption, sortedNewsData)}
+            {sortedNewsData.slice(startIndex, endIndex).map((news, index) => (
+              <a href={news.link}>
+                <div className="news-card mt-[70px] cursor-pointer" key={index}>
+                  <div className="img-box">
+                    <img className="img" src={Image} alt="" />
+                  </div>
+                  <div className="news-cat my-auto">
+                    <h5>研究活動レポート / 松村 浩由</h5>
+                  </div>
+                  <div className="mt-[30px]">
+                    <p>{truncateText(news.title, 80)}</p>
+                  </div>
+                  <div className="date-box flex mt-[90px] justify-between">
+                    <div>
+                      <p className="date">
+                        {new Date(news.isoDate).toLocaleDateString()}
+                      </p>
+                    </div>
+                    <div>
+                      {" "}
+                      <a href={news.link}>
+                        <h5 className="text-[10px] lg:text-[11px]">
+                          VIEW DETAILS
+                        </h5>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </a>
+            ))}
           </div>
-          </a>
-          ))}
         </div>
-      </div>
+      ) : (
+        <div>Loading...</div>
+      )}
 
-        {/* Pagination */}
-        <div className="pagination-box flex justify-center mb-2 w-[90%] m-auto">
-        {Array.from({ length: Math.ceil(newsData.length / itemsPerPage) }).map((_, index) => (
-            <a href="#card">
-          <button key={index} onClick={() => setCurrentPage(index + 1)} className="pagination mr-12">
-            {index + 1}
-          </button>
-          </a>
-        ))}
+      {/* Pagination */}
+      <div className="pagination-box flex justify-center mb-2 w-[90%] m-auto">
+        {Array.from({ length: Math.ceil(newsData.length / itemsPerPage) }).map(
+          (_, index) => (
+            <button
+              key={index}
+              onClick={() => {
+                setCurrentPage(index + 1);
+                scrollToTop();
+              }}
+              className={`pagination ${
+                currentPage === index + 1 ? "bold-text" : ""
+              } mr-12`}
+            >
+              {index + 1}
+            </button>
+          )
+        )}
       </div>
-
     </section>
   );
 }
--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -5,16 +5,16 @@ import linkIcon from "../../assets/images/link.jpeg"
 function Footer() {
   return (
     <section>
-      <div className=' bg-[#111111] w-[100%] lg:flex lg:p-8 gap-[300px]'>
+      <div className=' bg-[#111111] w-[100%] pl-[25px] lg:flex lg:p-8 gap-[300px]'>
       <div>
         <img className='w-[130px]' src={footerLogo} alt="footer logo" />
       </div>
       <div>
-        <ul className='flex gap-4 justify-center'>
-          <li className='text-white flex cursor-pointer'> プライバシーポリシー <img className='w-[25px] h-[20px] my-auto ml-[5px] cursor-pointer ' src={linkIcon} alt="icon1"/> </li>
-          <li className='text-white flex cursor-pointer'> 立命館大学（研究・産学連携） <img className='w-[25px] h-[20px] my-auto ml-[5px] cursor-pointer' src={linkIcon} alt="icon2"/> </li>
-          <li className='text-white flex cursor-pointer'> 立命館大学 研究部（学内者向け）<img className='w-[25px] h-[20px] my-auto ml-[5px] cursor-pointer' src={linkIcon} alt="icon3"/> </li>
-          <li className='text-white flex cursor-pointer'> 学園ビジョンR2030 <img className='w-[25px] h-[20px] my-auto ml-[5px] cursor-pointer' src={linkIcon} alt="icon4"/> </li>
+        <ul className='lg:flex gap-4 justify-center mt-[20px] lg:mt-auto'>
+          <li className='text-white text-[12px] lg:text flex cursor-pointer mt-4 lg:mt-auto'> プライバシーポリシー <img className='w-[25px] h-[20px] my-auto ml-[5px] cursor-pointer ' src={linkIcon} alt="icon1"/> </li>
+          <li className='text-white text-[12px] lg:text flex cursor-pointer mt-4 lg:mt-auto'> 立命館大学（研究・産学連携） <img className='w-[25px] h-[20px] my-auto ml-[5px] cursor-pointer' src={linkIcon} alt="icon2"/> </li>
+          <li className='text-white text-[12px] lg:text flex cursor-pointer mt-4 lg:mt-auto'> 立命館大学 研究部（学内者向け）<img className='w-[25px] h-[20px] my-auto ml-[5px] cursor-pointer' src={linkIcon} alt="icon3"/> </li>
+          <li className='text-white text-[12px] lg:text flex cursor-pointer mt-4 lg:mt-auto'> 学園ビジョンR2030 <img className='w-[25px] h-[20px] my-auto ml-[5px] cursor-pointer' src={linkIcon} alt="icon4"/> </li>
           
         </ul>
       </div>
