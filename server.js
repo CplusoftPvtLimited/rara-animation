114379d454823f23fdb984e26d43c4a5f977c32b
Abdullah Khan <khansaffy1@gmail.com>
2023-08-07 10:13:29 +0500

    create a stripe and coinbase payment api

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const sequelize = require('./config/db');
-
 const app = express();
 const PORT = process.env.PORT || 4500;
 const path = require('path');
@@ -12,12 +11,21 @@ sequelize.sync();
 const blogRoutes = require('./routes/blogRoutes');
 const categoryRoutes = require('./routes/categoryRoutes');
 const profileRoutes = require('./routes/profileRoutes');
+const checkoutRoute = require('./routes/checkRoute');
 
 // Middleware
-app.use(bodyParser.json());
+app.use(
+  bodyParser.json({
+    verify: (req, res, buf) => {
+      const url = req.originalUrl;
+      if (url.startsWith('/webhook')) {
+        req.rawBody = buf.toString();
+      }
+    },
+  })
+);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 //blog route
@@ -26,6 +34,8 @@ app.use('/api/blog', blogRoutes);
 app.use('/api/category', categoryRoutes);
 //Profile Route
 app.use('/api/profile', profileRoutes);
+//Stripe Payment Route
+app.use('/api/checkout', checkoutRoute);
 
 // Start the server
 app.listen(PORT, () => {
