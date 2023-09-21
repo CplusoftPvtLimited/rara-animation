3551bd2ee6370d50ce0d8e9216eaad22d747c15f
abdullahSaffy <khansaffy1@gmail.com>
2023-09-21 23:11:02 +0500

    solve bugs

--- src/components/Donation/donate/DonatePage.js
+++ src/components/Donation/donate/DonatePage.js
@@ -1,31 +1,18 @@
-import React from 'react';
+import React from "react";
 
 const DonatePage = () => {
   return (
     <div>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur
-        adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-        laboris nisi ut aliquip ex ea commodo consequat.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur
-        adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-        laboris nisi ut aliquip ex ea commodo consequat.
+        Your investment in the Pecunia Endowment Fund is not just a financial
+        commitment; it's a commitment to shaping the future of finance. By
+        joining our fund, you become a vital catalyst for positive change in the
+        world of finance and society at large. Your contributions, whether large
+        or small, fuel our mission to empower finance for a more inclusive,
+        equitable, and sustainable world. Together, we can harness the
+        transformative power of finance to create lasting impact. Invest today
+        and be part of the movement that's redefining finance for a brighter
+        tomorrow.
       </p>
     </div>
   );
