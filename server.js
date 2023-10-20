8b2a497c4c4ddb1cdeb7b8de8cf05b70b36a870d
Abdullah Khan <khansaffy1@gmail.com>
2023-10-20 12:11:31 +0500

    apply double string

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const sequelize = require('./config/db');
-const blogRoutes = require('./routes/blogRoutes');
-const profileRoutes = require('./routes/profileRoutes');
-const categoryRoutes = require('./routes/categoryRoutes');
-const checkoutRoute = require('./routes/checkRoute');
-const keysRoutes = require('./routes/keysRoutes');
-const coinbaseKeysRoutes = require('./routes/coinbaseKeysRoutes');
-const bankRoutes = require('./routes/bankRoutes');
-const RssFeedRoutes = require('./routes/RssFeedRoutes');
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const sequelize = require("./config/db");
+const blogRoutes = require("./routes/blogRoutes");
+const profileRoutes = require("./routes/profileRoutes");
+const categoryRoutes = require("./routes/categoryRoutes");
+const checkoutRoute = require("./routes/checkRoute");
+const keysRoutes = require("./routes/keysRoutes");
+const coinbaseKeysRoutes = require("./routes/coinbaseKeysRoutes");
+const bankRoutes = require("./routes/bankRoutes");
+const RssFeedRoutes = require("./routes/RssFeedRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 4500;
-const path = require('path');
+const path = require("path");
 sequelize.sync();
 
 // Middleware
@@ -22,7 +22,7 @@ app.use(
   bodyParser.json({
     verify: (req, res, buf) => {
       const url = req.originalUrl;
-      if (url.startsWith('/webhook')) {
+      if (url.startsWith("/webhook")) {
         req.rawBody = buf.toString();
       }
     },
@@ -30,26 +30,26 @@ app.use(
 );
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 //Blog Route
-app.use('/api/blog', blogRoutes);
+app.use("/api/blog", blogRoutes);
 //Category Route
-app.use('/api/category', categoryRoutes);
+app.use("/api/category", categoryRoutes);
 //Profile Route
-app.use('/api/profile', profileRoutes);
+app.use("/api/profile", profileRoutes);
 //Stripe Payment Route
-app.use('/api/checkout', checkoutRoute);
+app.use("/api/checkout", checkoutRoute);
 // stripe secrete keys routes
-app.use('/api/secret', keysRoutes);
+app.use("/api/secret", keysRoutes);
 // coinbase secrete keys routes
-app.use('/api/secret', coinbaseKeysRoutes);
+app.use("/api/secret", coinbaseKeysRoutes);
 
 // bank details
-app.use('/api/bank', bankRoutes);
+app.use("/api/bank", bankRoutes);
 
 // RSS Route
-app.use('/api/news', RssFeedRoutes);
+app.use("/api/news", RssFeedRoutes);
 
 // Start the server
 app.listen(PORT, () => {
